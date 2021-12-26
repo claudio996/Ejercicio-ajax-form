1a02135986de849ce1afbc8fa42ef118da9af5d6
claudio996 <claudioantonio.retamal@gmail.com>
2021-12-25 22:37:48 -0300

    upload-server

--- main.js
+++ main.js
@@ -1,37 +1,67 @@
-/*get document for user - prepared solicitude  - process-server - send-response to the fronted */
 const d = document,
-    $main = d.querySelector(".main"),
+    $main = d.querySelector("main"),
     $files = d.getElementById('files');
 
 const uploader = (file) => {
     const xhr = new XMLHttpRequest(); //object xhr.
-
     const formData = new FormData(); //simulated forms.
     formData.append('file', file);
-
-    //Petition to the server.
-    xhr.open("POST", "server/uploader.php")//open peticion for procesing server.
-    xhr.setRequestHeader("enc-type", "multipart/form-data")//creating headers for data extriction (especified extrict type of  files for procesed)  
-    xhr.send(formData); //send you forms. 
 
     xhr.addEventListener('readystatechange', e => { //listening status.
         if (xhr.readyState !== 4) return; //validate  code errorStatus.
 
         if (xhr.status >= 200 && xhr.status < 300) { //validate code status ok.
             let json = JSON.parse(xhr.responseText); //parse response,
+
             console.log(json);
         } else {
             let message = xhr.statusText;
             console.log(message);
         }
+
+
+    });
+
+    //Petition to the server.
+    xhr.open("POST", "server/uploader.php")//open peticion for procesing server.
+    xhr.setRequestHeader("enc-type", "multipart/form-data")//creating headers for data extriction (especified extrict type of  files for procesed)  
+    xhr.send(formData); //send you forms. 
+}
+
+const progressUpload = (file) => {
+    const $progress = d.createElement("progress"), //creating elements.
+        $span = d.createElement("span");
+
+    $progress.value = 0; //add values
+    $progress.max = 100;
+    $main.insertAdjacentElement("beforeend", $progress); //insert elements 
+    $main.insertAdjacentElement("beforeend", $span);
+
+    const fileReader = new FileReader();  //creating ibject fileReader
+    fileReader.readAsDataURL(file);
+
+    fileReader.addEventListener("progress", e => {
+        //
+        let progress = parseInt((e.loaded * 100) / e.total); 
+        $progress.value = progress; //set values.
+        $span.innerHTML = `<b>${file.name}- ${progress} %</b>`;
+    });
+
+    fileReader.addEventListener("loadend", e => {
+        //upload info in server.
+
+        uploader(file);
+        setTimeout(() => {
+            $main.removeChild($progress),
+                $main.removeChild($span);
+            $files.value = ""
+        }, 3000);
     });
 }
 
 d.addEventListener("change", e => {//Get files input.
     if (e.target === $files) {
         const files = Array.from(e.target.files); // get all files. with foreach in method Array.
-        files.forEach(element => uploader(element))
+        files.forEach(element => progressUpload(element))
     }
 })
-
-
